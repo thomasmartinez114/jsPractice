a76c3b26b02aeb39b4d96d16b1aa1789a2ad7385
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-28 21:32:42 -0400

    added script and index

---
+++ examples.js
@@ -0,0 +1,24 @@
+// For Loop
+for(var i = 0; i < 10; i++){
+	console.log(i)
+}
+
+// For Each
+const arr = [1, 2, 3]
+
+arr.forEach(function(element){
+	console.log(element + 1)
+})
+
+// Map
+arr
+
+arr.map(function(element){
+    return element + 1
+})
+
+const newArr = arr.map(function(element){
+    return element + 1
+})
+
+newArr
