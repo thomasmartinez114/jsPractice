b30ad8c6e1533096295f5189e4e0174626fa64f0
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-06-01 12:49:44 -0400

    completed all assignments

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
 
-function runScript() {
-    // 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
-    // Using a for loop:
-    var result1a = [];
-    for (var i = 0; i < numbers.length; i++) {
-        const character = String.fromCharCode(numbers[i]);
-        result1a.push(character);
+// 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
+
+// Using a for loop:
+function answerLoop() {
+    var array = [];
+    for (var i = 0; i < numbers.length; i++){
+        const alpha = String.fromCharCode(numbers[i]);
+        array.push(alpha);
     }
-
-    document.getElementById("1a").innerHTML += " " + result1a.join(", ");
-    // Using the forEach method:
-    // Using the map method:
-
-    // 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
-    // Answer:
-
-    // 3. Display the product of all numbers using reduce
-    // Answer:
+    document.getElementById('loop').innerHTML += " " + array.join(", ");
 }
+
+
+// Using the forEach method:
+function answerForEach() {
+    numbers.forEach(e => {
+        document.getElementById('forEach').innerHTML += " " + ", " + String.fromCharCode(e)
+    })
+}
+
+// Using the map method:
+function answerMap() {
+    let alpha = numbers.map(numbers => {
+        document.getElementById('map').innerHTML += ' ' + String.fromCharCode(numbers)
+    })
+}
+
+// 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
+
+// Answer:
+function greaterThan() {
+    const filtered = numbers.filter(e => {
+        return e > 72 && e < 88
+    })
+    document.getElementById('numbersLowGreat').innerHTML += ' ' + filtered.join(' ')
+}
+
+// 3. Display the product of all numbers using reduce
+// Answer:
+function reduceProduct() {
+    const product = numbers.reduce((acc, cur) => {
+        return acc * cur
+    })
+    document.getElementById('reduced').innerHTML += ' ' + product
+}
+
