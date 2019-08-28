5170d67f11d203d0882afd7ac3161344da4b5d5a
Thomas Martinez <48811054+thomasmartinez114@users.noreply.github.com>
2019-08-27 21:04:38 -0400

    need to finish Brand

--- class-walkthrough/vehicle.js
+++ class-walkthrough/vehicle.js
@@ -1,1 +1,28 @@
 // Vehicle > Sedan/HatchBack > Brand
+
+class Vehicle {
+  constructor(fuel, color, tint) {
+    this.fuel = fuel;
+    this.color = color;
+    this.tint = tint;
+    this.wheels = 4;
+  }
+}
+
+class Sedan extends Vehicle {
+  constructor(fuel, color, tint, doors, seats) {
+    super(fuel, color, tint); // inherit props from Vehicle
+    this.doors = doors;
+    this.seats = seats;
+  }
+}
+
+class HatchBack extends Vehicle {
+  constructor(fuel, color, tint, doors, seats) {
+    super(fuel, color, tint); // inherit props from Vehicle
+    this.doors = doors;
+    this.seats = seats;
+  }
+}
+
+class Brand extends
