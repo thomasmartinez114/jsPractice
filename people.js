559b4c1ea3e7f1c3b1413154f8628a20238cb725
Thomas Martinez <48811054+thomasmartinez114@users.noreply.github.com>
2019-08-27 20:39:03 -0400

    updated class walkthrough

--- people.js
+++ people.js
@@ -4,7 +4,8 @@ class Person {
   constructor(name, age, height) {
     this.name = name;
     this.age = age;
-    this.height = undefined;
+    this.height = height;
+    this.planet = 'Earth';
   }
 
   changeName(newName) {
@@ -26,3 +27,32 @@ class Person {
 // console.log(jason);
 // jason.birthday();
 // console.log(jason.age);
+
+class Employee extends Person {
+  constructor(name, age, height) {
+    super(name, age, height); // inherit all of the properties from the paramaters in Person
+  }
+}
+
+// const newEmployee = new Employee('Jason', 30, 190);
+// console.log(newEmployee.name);
+// console.log(newEmployee.planet);
+// console.log(newEmployee.height);
+// console.log(newEmployee.age);
+
+class Student extends Person {
+  constructor(name, age, height, id) {
+    super(name, age, height);
+    this.id = id;
+  }
+}
+
+const newStudent = new Student('Thomas', 67, 450, 002476);
+console.log(newStudent.name);
+console.log(newStudent.age);
+console.log(newStudent.height);
+console.log(newStudent.id);
+console.log(newStudent.planet);
+
+newStudent.birthday();
+console.log(newStudent.age);
