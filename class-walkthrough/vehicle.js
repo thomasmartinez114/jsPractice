dc39b5559cf0206ab06ff7f23113c6101de0d2c5
Thomas Martinez <48811054+thomasmartinez114@users.noreply.github.com>
2019-08-28 17:05:10 -0400

    undefined for the Type class --- can extend two classes?

--- class-walkthrough/vehicle.js
+++ class-walkthrough/vehicle.js
@@ -10,13 +10,39 @@ class Vehicle {
 }
 
 class Type extends Vehicle {
-    constructor(fuel, color, tint, doors, seats, style) {
-        super(fuel, color, tint); // inherit the props from Vehicle
-    }
+  constructor(fuel, color, tint, doors, seats, style) {
+    super(fuel, color, tint); // inherit the props from Vehicle
+    this.doors = doors;
+    this.seats = seats;
+    this.style = style;
+  }
 }
 
-class Brand extends Type, Vehicle {
-    constructor(fuel, color, tint, doors, seats, style) {
-        super(fuel, color, tint, doors, seats, style);
-    }
+class Brand extends Vehicle {
+  constructor(fuel, color, tint, doors, seats, style, make, model) {
+    super(fuel, color, tint, doors, seats, style);
+    this.make = make;
+    this.model = model;
+  }
 }
+
+const newBrand = new Brand(
+  'Premium',
+  'Red',
+  false,
+  4,
+  6,
+  'Sedan',
+  'Corvette',
+  'Stingray'
+);
+console.log(newBrand.fuel);
+console.log(newBrand.color);
+console.log(newBrand.tint);
+console.log(newBrand.doors);
+console.log(newBrand.seats);
+console.log(newBrand.style);
+console.log(newBrand.make);
+console.log(newBrand.model);
+
+console.log(newBrand);
